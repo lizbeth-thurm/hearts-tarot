bcd0ed5f4583d419df195d2fd6b775dadedd01a8
slthurman01 <slthurman01@gmail.com>
2023-04-14 07:36:17 -0500

    Creates react app, adds draggable component package

---
+++ hearts-tarot/src/App.js
@@ -0,0 +1,31 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Draggable from "react-draggable";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Draggable>
+          <div className="box">
+            <div>Move me around!</div>
+          </div>
+        </Draggable>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
